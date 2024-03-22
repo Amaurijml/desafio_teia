36a472feac3205ac2ce4a0043f1fe4a59183ceff
Amaurijml <amaurijml@gmail.com>
2024-03-22 10:10:12 -0300

    Desafio Caixa
    Desafio Caixa desenvolvido em Angular V17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,74 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataService } from './service/data.service';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+interface Post {
+  albumId: number;
+  id: number;
+  title: string;
+  url : string;
+  thumbnailUrl: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatPaginatorModule,MatExpansionModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'desafio_teia';
+  title = 'ng-client';
+  photos: Post[] = [];
+  errorMessage!: string;
+ // certo até aqui
+
+totalItems = 5000;
+pageSize = 10;
+currentPage = 0;
+
+constructor(private data_service: DataService) {}
+
+items =  this.data_service.getAllPosts(this.currentPage, this.pageSize);
+
+pageChanged(event: PageEvent) {
+
+  this.currentPage = event.pageIndex ;
+  console.log(this.currentPage);
+  console.log((this.currentPage  * (this.pageSize) +" "+ this.pageSize));
+  this.items = this.data_service.getAllPosts(this.currentPage * (this.pageSize), this.pageSize);
+
+
+ // this.items = this.data_service.getAllPosts(this.currentPage, this.pageSize);
+  this.items.subscribe({
+    next: (photos) => {
+      this.photos = photos;
+      //console.log(this.photos);
+    },
+    error: (error) => {
+      this.errorMessage = error;
+    },
+  });
+ 
+ //console.log("pagina" + this.currentPage);
+}
+
+ 
+
+  ngOnInit() {
+    this.items.subscribe(result => {console.log(result.length)});
+    this.items.subscribe({
+      next: (photos) => {
+        this.photos = photos;
+        console.log((this.currentPage + (this.pageSize -1) +" "+ this.pageSize));
+      //  console.log(this.photos);
+      },
+      error: (error) => {
+        this.errorMessage = error;
+      },
+    });
+  }
 }
---
+++ src/app/service/data.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient , HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+interface Post {
+  albumId: number;
+  id: number;
+  title: string;
+  url : string;
+  thumbnailUrl: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  apiUrl = 'https://jsonplaceholder.typicode.com/photos/';
+
+ 
+
+  constructor(private http: HttpClient) {}
+
+  getAllPosts(offset?: number, pageSize?: number): Observable<Post[]> {
+    const params = new HttpParams({
+      fromObject: {
+        _start: offset ?? 0,
+        _limit: pageSize ?? 10,
+      },
+    });
+
+    return this.http.get<Post[]>(this.apiUrl,{ params});
+  }
+
+}
+
+
+
+
+
+
+
+
+// funcionando ate aqui 2
+/*
+this.currentPage, this.pageSize
+getTodoItems(offset?: number, pageSize?: number): Observable<TodoItem[]> {
+  const params = new HttpParams({
+    fromObject: {
+      _start: offset ?? 0,
+      _limit: pageSize ?? 10,
+    },
+  });
+
+  return this._http.get<TodoItem[]>(
+    "https://jsonplaceholder.typicode.com/todos",
+    { params }
+*/
