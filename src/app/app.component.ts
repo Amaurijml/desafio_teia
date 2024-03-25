fc8a412c8b51df5782e0cf9f8f5f9097071f6348
Amaurijml <amaurijml@gmail.com>
2024-03-24 21:31:59 -0300

    Implementação
    Implementação busca por albumId e id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {MatTableModule} from '@angular/material/table';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import _ from 'lodash';
 
 
 
@@ -24,21 +25,27 @@ interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
+
 export class AppComponent {
   title = 'ng-client';
   photos: Post[] = [];
+  para_agrupar : Post[] = [];
   errorMessage!: string;
  // certo até aqui
 
+ 
 totalItems = 5000;
 pageSize = 10;
 currentPage = 0;
-
+grouped : any;
+groupedId: any;
 constructor(private data_service: DataService) {}
 
 items =  this.data_service.getAllPosts(this.currentPage, this.pageSize);
+item_todos = this.data_service.getTodos();
 
-
+ 
 
 pageChanged(event: PageEvent) {
 
@@ -47,30 +54,65 @@ pageChanged(event: PageEvent) {
   console.log((this.currentPage  * (this.pageSize) +" "+ this.pageSize));
   this.items = this.data_service.getAllPosts(this.currentPage * (this.pageSize), this.pageSize);
 
-
- // this.items = this.data_service.getAllPosts(this.currentPage, this.pageSize);
   this.items.subscribe({
     next: (photos) => {
       this.photos = photos;
-      //console.log(this.photos);
-    },
+      },
     error: (error) => {
       this.errorMessage = error;
     },
   });
  
- //console.log("pagina" + this.currentPage);
 }
 
+buscaAlbum(album: string){
+ let albumx = parseFloat(album)
+  this.grouped =  this.para_agrupar.filter((x: { albumId: number; }) => x.albumId === albumx);
+ // console.log( this.grouped[0].albumId)
+}
+ 
+buscaId(id: string){
+  let albumy = parseFloat(id)
+  this.groupedId =  this.para_agrupar.filter((x: {id: number; }) => x.id === albumy);
+  console.log( this.grouped)
+}
  
 
   ngOnInit() {
-    this.items.subscribe(result => {console.log(result.length)});
+
+    this.item_todos.subscribe(
+      {
+        next: (para_agrupar) => {
+          this.para_agrupar = para_agrupar;
+       //   this.grouped = _.groupBy(this.para_agrupar, 'albumId');
+
+       // console.log( this.grouped[2][0]);
+
+   //     this.grouped =  this.para_agrupar.filter((x: { id: number; }) => x.id === 53);
+   //     console.log( this.grouped);
+    //    this.grouped =  this.para_agrupar.filter((x: { albumId: number; }) => x.albumId === 50);
+   //     console.log( this.grouped);
+        
+       // this.grouped = this.grouped.filter((x: { id: number; }) => x.id === 2);
+     //   console.log( this.grouped);
+        
+        },
+        error: (error) => {
+          this.errorMessage = error;
+        },
+        
+      }
+    );
+      
+
     this.items.subscribe({
       next: (photos) => {
         this.photos = photos;
-        console.log((this.currentPage + (this.pageSize -1) +" "+ this.pageSize));
-      //  console.log(this.photos);
+       // console.log((this.currentPage + (this.pageSize -1) +" "+ this.pageSize));
+
+       // this.grouped = this.photos.filter((x: { id: number; }) => x.id === 3);
+       // console.log( this.grouped);
+       // console.log(  this.photos);
       },
       error: (error) => {
         this.errorMessage = error;
